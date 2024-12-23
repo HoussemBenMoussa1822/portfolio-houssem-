69a9695364d1f562f12d323dd0bcf22fcece5004
samer-mansouri <samer.mansouri@insat.ucar.tn>
2024-12-23 17:38:51 +0100

    test 01

--- app/pages/layout.js
+++ app/pages/layout.js
@@ -1,12 +1,11 @@
-import { GoogleTagManager } from "@next/third-parties/google";
 import { Inter } from "next/font/google";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Footer from "./components/footer";
-import Navbar from "./components/navbar";
-import "./css/card.scss";
-import "./css/globals.scss";
-import ScrollToTop from "./components/helper/scroll-to-top";
+import Footer from "../components/footer";
+import Navbar from "../components/navbar";
+import "../css/card.scss";
+import "../css/globals.scss";
+import ScrollToTop from "../components/helper/scroll-to-top";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
