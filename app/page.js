bdee95e2d1d4ace9de7d2ae32022751c3179a3bd
hbenmoussa <Stage_DE@smarte-conseil.com>
2024-12-24 15:57:20 +0100

    Portfolio Houssi

--- app/page.js
+++ app/page.js
@@ -1,14 +1,15 @@
-import AboutSection from "./components/homepage/about";
-import ContactSection from "./components/homepage/contact";
-import Education from "./components/homepage/education";
-import Experience from "./components/homepage/experience";
-import HeroSection from "./components/homepage/hero-section";
-import Projects from "./components/homepage/projects";
-import Skills from "./components/homepage/skills";
+import dynamic from 'next/dynamic';
 
+// Charger dynamiquement certains composants avec SSR désactivé si nécessaire
+const HeroSection = dynamic(() => import("./components/homepage/hero-section"), { ssr: false });
+const AboutSection = dynamic(() => import("./components/homepage/about"), { ssr: false });
+const Experience = dynamic(() => import("./components/homepage/experience"), { ssr: false });
+const Skills = dynamic(() => import("./components/homepage/skills"), { ssr: false });
+const Projects = dynamic(() => import("./components/homepage/projects"), { ssr: false });
+const Education = dynamic(() => import("./components/homepage/education"), { ssr: false });
+const ContactSection = dynamic(() => import("./components/homepage/contact"), { ssr: false });
 
 export default function Page() {
-
   return (
     <>
       <HeroSection />
@@ -19,5 +20,5 @@ export default function Page() {
       <Education />
       <ContactSection />
     </>
-  )
+  );
 };
