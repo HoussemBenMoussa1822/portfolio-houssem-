740907f2fc368ce9c67a616d94f57522cf4e136a
hbenmoussa <Stage_DE@smarte-conseil.com>
2024-12-24 16:02:40 +0100

    Portfolio Houss

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 import dynamic from 'next/dynamic';
-
-// Charger dynamiquement certains composants avec SSR désactivé si nécessaire
+// Charger dynamiquement les composants avec ssr: false pour les bibliothèques ou composants manipulant le DOM
 const HeroSection = dynamic(() => import("./components/homepage/hero-section"), { ssr: false });
 const AboutSection = dynamic(() => import("./components/homepage/about"), { ssr: false });
 const Experience = dynamic(() => import("./components/homepage/experience"), { ssr: false });
@@ -8,6 +7,9 @@ const Skills = dynamic(() => import("./components/homepage/skills"), { ssr: fals
 const Projects = dynamic(() => import("./components/homepage/projects"), { ssr: false });
 const Education = dynamic(() => import("./components/homepage/education"), { ssr: false });
 const ContactSection = dynamic(() => import("./components/homepage/contact"), { ssr: false });
+
+// Charger lottie-react dynamiquement pour éviter l'erreur côté serveur
+const LottieReact = dynamic(() => import('lottie-react'), { ssr: false });
 
 export default function Page() {
   return (
